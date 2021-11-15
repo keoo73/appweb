7941f57e2e83a4222c98c2111727691f75dd8ed1
EDZA09 <eduarstiven2001@gmail.com>
2021-11-15 09:01:00 -0500

    Diseñando servicio de autenticacion

--- src/services/autenticacion.service.ts
+++ src/services/autenticacion.service.ts
@@ -1,4 +1,6 @@
-import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import {/* inject, */ BindingScope, injectable} from '@loopback/core';
+const generador = require('password-generator');
+const cryptoJS = require('crypto-js');
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
@@ -7,4 +9,17 @@ export class AutenticacionService {
   /*
    * Add service methods here
    */
+
+  generarContrasenia() {
+    //param1 = longitud de la contraseña
+    //param2 = intensidad de la clav, True = mas compleja
+    //generador(param1, param2)
+    const contrasenia = generador(8, false);
+    return contrasenia;
+  }
+
+  cifrarContrasenia(contrasenia: string) {
+    const contraseniaCifrada = cryptoJS.MD5(contrasenia).toString();
+    return contraseniaCifrada;
+  }
 }
