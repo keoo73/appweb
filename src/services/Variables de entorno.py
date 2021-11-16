c606a1d75c4bd07f9f8675da3d33d7e23f126d27
EDZA09 <eduarstiven2001@gmail.com>
2021-11-16 14:40:45 -0500

    Subiendo archivos de Servicios local - Twilio y SendGrid

---
+++ src/services/Variables de entorno.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 20:56:40 2021
+
+@author: USER
+"""
+
+import os
+
+os.environ['TWILIO_ACCOUNT_SID']='ACa9828af590f194593c3b7d8ffef14565'
+os.environ['TWILIO_AUTH_TOKEN']='f92b35a18913c5fcddda1ddb7e2da079'
+os.environ['SENDGRID_API_KEY']='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'
+os.environ['Test']='Prueba de Variable de Entorno'
