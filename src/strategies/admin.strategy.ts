acdb2301c6d1d52c292264b2d221e3b50bcfbead
EDZA09 <eduarstiven2001@gmail.com>
2021-11-23 21:58:20 -0500

    Registrando servicio de autenticacion y estrategia

---
+++ src/strategies/admin.strategy.ts
@@ -0,0 +1,39 @@
+import {AuthenticationStrategy} from '@loopback/authentication';
+import {service} from '@loopback/core';
+import {HttpErrors, Request} from '@loopback/rest';
+import {UserProfile} from '@loopback/security';
+import parseBearerToken from 'parse-bearer-token';
+import {AutenticacionService} from '../services';
+
+export class EstrategiaAdministrador implements AuthenticationStrategy {
+  name = 'admin';
+
+  /**
+   *
+   */
+  constructor(
+    @service(AutenticacionService)
+    public servicioAutenticacion: AutenticacionService,
+  ) {}
+
+  async authenticate(request: Request): Promise<UserProfile | undefined> {
+    const token = parseBearerToken(request);
+    if (token) {
+      const datos = this.servicioAutenticacion.validarTokenJWT(token);
+      if (datos) {
+        if (datos.data) {
+          //Aquí se podría tener en cuenta el
+          const perfil: UserProfile = Object.assign({
+            nombre: datos.data.nombre,
+            rol: datos.data.rol,
+          });
+          return perfil;
+        }
+      } else {
+        throw new HttpErrors[401]('El token incluido no es válido');
+      }
+    } else {
+      throw new HttpErrors[401]('No se ha incluido un token en la solicitud');
+    }
+  }
+}
