497675ad652ac3d4ecf37a3d1c9d8fd959015836
keoo73 <37909164+keoo73@users.noreply.github.com>
2021-12-08 20:39:36 -0500

    Update Variables de entorno.py

--- src/services/Variables de entorno.py
+++ src/services/Variables de entorno.py
@@ -7,7 +7,7 @@ Created on Thu Nov 11 20:56:40 2021
 
 import os
 
-os.environ['TWILIO_ACCOUNT_SID']='ACc14aa33ac5e5f67231b901a32b008a12'
-os.environ['TWILIO_AUTH_TOKEN']='6004a82a14fc25cf4791f33ec5c54254'
-os.environ['SENDGRID_API_KEY']='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'
+os.environ['TWILIO_ACCOUNT_SID']=xxxxxxx
+os.environ['TWILIO_AUTH_TOKEN']='xxxxxxx
+os.environ['SENDGRID_API_KEY']=xxxxxxx
 os.environ['Test']='Prueba de Variable de Entorno'
